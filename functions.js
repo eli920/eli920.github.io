4e2ae6bc4aa6605f2cd3ecae2a347a88bf1a7c01
eli920 <126629599+eli920@users.noreply.github.com>
2023-03-01 01:06:52 -0300

    Compromiso inicial

---
+++ functions.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento click");
+    alert("Gracias! dirijase a la hoja de contacto e ingrese sus datos.");
+}
